6d5026ebd94a39f428704ea05218815706f7a992
Shubham_Infinity <shubham_infinity@Shubham-Infinitys-MacBook-Air.local>
2024-07-31 22:06:52 +0530

    End Of Section 6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, inject, input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
   private accountSService = inject(AccountService);
+  private toastr = inject(ToastrService);
 
   @Output() cancelRegister = new EventEmitter();
   //cancelRegister=output<boolean>();
@@ -21,7 +23,7 @@ export class RegisterComponent {
         console.log(response);
         this.cancel();
       },
-      error: (error) => console.log(error),
+      error: (error) => this.toastr.error(error.error),
     });
   }
   cancel() {
