22851468c9a0c01ef11ad0967d1fb5e5a29ab437
Shubham_Infinity <shubham_infinity@Shubham-Infinitys-MacBook-Air.local>
2024-08-02 20:44:16 +0530

    End Of Section 7

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localHost:4200", "https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
